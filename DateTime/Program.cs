24b9a78c542ef1b0fcc514bbed33eeed643f66f7
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-06 17:13:31 +0300

    Proje dosyası ekle.

---
+++ DateTime/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //kullanıcıdan doğum tarihi alınacak ve yaşı hesaplanıp mesaj verilecek
+            int fark,girilenyil;
+            string mesaj;
+            int suankiyil = Convert.ToInt32(DateTime.Now.ToString("%y"));
+            Console.WriteLine();
+            suankiyil += 2000;
+            Console.WriteLine("{0} yılındayız",suankiyil);
+            Console.WriteLine("Doğum yılınızı yazınız:");
+            girilenyil = Convert.ToInt32(Console.ReadLine());
+            fark = suankiyil - girilenyil;
+            if (fark < 20)
+            {
+                mesaj = "Ergensin:D";
+            }
+            else if (fark < 30)
+            {
+                mesaj = "Gençsiniz:D";
+            }
+            else if (fark < 40)
+            {
+                mesaj = "Yaşlanıyorsunuz:(";
+            }
+            else
+            {
+                mesaj = "Yaşlısınız";
+
+            }
+            Console.WriteLine(mesaj);
+
+            Console.ReadLine();
+        }
+    }
+}
